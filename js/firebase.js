0eb34942947a1a9ff60f1159b9bdbf38a6a22d9e
rsemon095-fixture <rsemon095@gmail.com>
2026-07-11 21:27:29 +0600

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,20 +1,24 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-app.js";
 
-// Your web app's Firebase configuration
+import { getDatabase } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-database.js";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCzUqBtLR3MgmFDSb71leHZrNGZhRVU_-o",
-  authDomain: "matelecom-data-entry.firebaseapp.com",
-  projectId: "matelecom-data-entry",
-  storageBucket: "matelecom-data-entry.firebasestorage.app",
-  messagingSenderId: "1020949745883",
-  appId: "1:1020949745883:web:bd9c51f11dc37bacf43cfe"
-};
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+  apiKey: "AIzaSyCzUqBtLR3MgmFDSb71leHZrNGZhRVU_-o",
+
+  authDomain: "matelecom-data-entry.firebaseapp.com",
+
+  databaseURL: "https://matelecom-data-entry-default-rtdb.firebaseio.com",
+
+  projectId: "matelecom-data-entry",
+
+  storageBucket: "matelecom-data-entry.firebasestorage.app",
+
+  messagingSenderId: "1020949745883",
+
+  appId: "1:1020949745883:web:bd9c51f11dc37bacf43cfe"
+
+};
 
 const app = initializeApp(firebaseConfig);
 
