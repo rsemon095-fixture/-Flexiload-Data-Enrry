ed3db06a91ae4913d2883214fcb698074a1744f8
rsemon095-fixture <rsemon095@gmail.com>
2026-07-11 21:11:37 +0600

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,23 +1,20 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-app.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-database.js";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-
-  apiKey: "YOUR_API_KEY",
-
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-
-  databaseURL: "https://YOUR_PROJECT-default-rtdb.firebaseio.com",
-
-  projectId: "YOUR_PROJECT",
-
-  storageBucket: "YOUR_PROJECT.appspot.com",
-
-  messagingSenderId: "YOUR_SENDER_ID",
-
-  appId: "YOUR_APP_ID"
-
+  apiKey: "AIzaSyCzUqBtLR3MgmFDSb71leHZrNGZhRVU_-o",
+  authDomain: "matelecom-data-entry.firebaseapp.com",
+  projectId: "matelecom-data-entry",
+  storageBucket: "matelecom-data-entry.firebasestorage.app",
+  messagingSenderId: "1020949745883",
+  appId: "1:1020949745883:web:bd9c51f11dc37bacf43cfe"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 const app = initializeApp(firebaseConfig);
 
