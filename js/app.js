2b1c670dcbdbbc9b9c33ef5b17d5dec9a6a0cf85
rsemon095-fixture <rsemon095@gmail.com>
2026-07-12 00:20:03 +0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,68 @@
-// Loading শেষ হলে PIN Page-এ যাবে
+let percent = 0;
 
-setTimeout(function () {
-    window.location.href = "pin.html";
-}, 3000);
+const percentText = document.getElementById("percent");
+const fill = document.getElementById("loadingFill");
+
+const loadingText = document.querySelector(".loading-text");
+
+const textList = [
+
+"Initializing System...",
+
+"Loading Database...",
+
+"Connecting Firebase...",
+
+"Loading Dashboard...",
+
+"Almost Ready..."
+
+];
+
+let txt = 0;
+
+const timer = setInterval(() => {
+
+    percent++;
+
+    fill.style.width = percent + "%";
+
+    percentText.innerHTML = percent + "%";
+
+    if(percent==20){
+
+        loadingText.innerHTML=textList[1];
+
+    }
+
+    if(percent==40){
+
+        loadingText.innerHTML=textList[2];
+
+    }
+
+    if(percent==60){
+
+        loadingText.innerHTML=textList[3];
+
+    }
+
+    if(percent==80){
+
+        loadingText.innerHTML=textList[4];
+
+    }
+
+    if(percent>=100){
+
+        clearInterval(timer);
+
+        setTimeout(()=>{
+
+            window.location.href="dashboard.html";
+
+        },500);
+
+    }
+
+},30);
