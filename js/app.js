b74a56871266f3b78e175ad4ce656a0592c7f1a2
rsemon095-fixture <rsemon095@gmail.com>
2026-07-12 01:50:05 +0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,34 +1,5 @@
-let percent = 0;
+// Loading শেষ হলে PIN Page-এ যাবে
 
-const percentText = document.getElementById("percent");
-const fill = document.getElementById("loadingFill");
-const loadingText = document.querySelector(".loading-text");
-
-const textList = [
-    "Initializing System...",
-    "Loading Database...",
-    "Connecting Firebase...",
-    "Loading Dashboard...",
-    "Almost Ready..."
-];
-
-const timer = setInterval(() => {
-
-    percent++;
-
-    fill.style.width = percent + "%";
-    percentText.innerHTML = percent + "%";
-
-    if (percent == 20) loadingText.innerHTML = textList[1];
-    if (percent == 40) loadingText.innerHTML = textList[2];
-    if (percent == 60) loadingText.innerHTML = textList[3];
-    if (percent == 80) loadingText.innerHTML = textList[4];
-
-    if (percent >= 100) {
-
-        clearInterval(timer);
-
-        window.location.href = "dashboard.html";
-    }
-
-}, 30);
+setTimeout(function () {
+    window.location.href = "pin.html";
+}, 3000);
