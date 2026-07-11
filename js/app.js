1abf675fe2578c0d4575bccb6518bb74aeb5cf34
rsemon095-fixture <rsemon095@gmail.com>
2026-07-12 01:35:47 +0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,34 @@
-setTimeout(()=>{
-    window.location.href="dashboard.html";
-},3000);
+let percent = 0;
+
+const percentText = document.getElementById("percent");
+const fill = document.getElementById("loadingFill");
+const loadingText = document.querySelector(".loading-text");
+
+const textList = [
+    "Initializing System...",
+    "Loading Database...",
+    "Connecting Firebase...",
+    "Loading Dashboard...",
+    "Almost Ready..."
+];
+
+const timer = setInterval(() => {
+
+    percent++;
+
+    fill.style.width = percent + "%";
+    percentText.innerHTML = percent + "%";
+
+    if (percent == 20) loadingText.innerHTML = textList[1];
+    if (percent == 40) loadingText.innerHTML = textList[2];
+    if (percent == 60) loadingText.innerHTML = textList[3];
+    if (percent == 80) loadingText.innerHTML = textList[4];
+
+    if (percent >= 100) {
+
+        clearInterval(timer);
+
+        window.location.href = "dashboard.html";
+    }
+
+}, 30);
